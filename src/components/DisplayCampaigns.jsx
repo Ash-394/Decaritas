6c989eae003a6b6d5a85d75d2b146d36b3696b93
Ash-394 <aswini.at100@gmail.com>
2024-03-10 11:43:24 +0530

    scroll

--- src/components/DisplayCampaigns.jsx
+++ src/components/DisplayCampaigns.jsx
@@ -85,7 +85,7 @@ const DisplayCampaigns = ({ contract }) => {
     return formattedDate;
   };
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 scrollbar-none">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 overflow-y-scroll ">
       {campaigns.map((campaign, index) => (
         <div key={index} className="bg-white shadow-md rounded-lg p-4">
 
