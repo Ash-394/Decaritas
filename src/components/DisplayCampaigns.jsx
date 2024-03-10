a789500f58adb863c55d03fd6e4e0041b74c75d5
Ash-394 <aswini.at100@gmail.com>
2024-03-10 11:21:16 +0530

    upload proof

--- src/components/DisplayCampaigns.jsx
+++ src/components/DisplayCampaigns.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 
@@ -6,6 +5,8 @@ const DisplayCampaigns = ({ contract }) => {
   const [campaigns, setCampaigns] = useState([]);
   const [value, setValue] = useState('0');
   const [donationValues, setDonationValues] = useState({});
+  const [proofImages, setProofImages] = useState({}); // State to store proof images for each campaign
+
   useEffect(() => {
     const fetchCampaigns = async () => {
       try {
@@ -21,7 +22,22 @@ const DisplayCampaigns = ({ contract }) => {
     fetchCampaigns();
   }, [contract]); // Execute useEffect whenever contract changes
 
+  const handleProofImageUpload = (campaignId, event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
 
+    reader.onloadend = () => {
+      const proofImageUrl = reader.result;
+      setProofImages(prevProofImages => ({
+        ...prevProofImages,
+        [campaignId]: proofImageUrl,
+      }));
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
   const handleDonationChange = (index, value) => {
     // Update donation value for the selected campaign
     setDonationValues({
@@ -68,7 +84,6 @@ const DisplayCampaigns = ({ contract }) => {
     const formattedDate = `${year}-${month < 10 ? '0' + month : month}-${date < 10 ? '0' + date : date} ${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
     return formattedDate;
   };
-
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 scrollbar-none">
       {campaigns.map((campaign, index) => (
@@ -84,19 +99,28 @@ const DisplayCampaigns = ({ contract }) => {
           <p className="text-sm text-gray-500 mb-2">Target : {ethers.formatUnits(campaign.target)} </p>
           <p className="text-sm text-gray-500 mb-2"> Deadline: {formatTimestamp(campaign.deadline.toString())} </p>
 
-          <div className="flex items-center">
-            <label className="mr-2">Amount(ETH):</label>
-            <input
-              className="border border-gray-300 rounded px-2 py-1 mr-2"
-              value={donationValues[index]}
-              onChange={(e) => handleDonationChange(index, e.target.value)}
-            />
-    
-          </div>
+          <label htmlFor={`proofImage${index}`} className="block text-sm font-medium text-gray-700">
+  Upload Proof
+</label>
+<input
+  type="file"
+  id={`proofImage${index}`}
+  name={`proofImage${index}`}
+  accept="image/*"
+  onChange={(e) => handleProofImageUpload(index, e)}
+  className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+/>
+
+          {/* Display uploaded proof image */}
+          {proofImages[index] && (
+            <img src={proofImages[index]} alt="Proof" className="max-w-auto h-[200px] mt-4" />
+          )}
+
+          {/* Buttons for donating and withdrawing */}
           <div className='mt-3 mb-3 flex items-center justify-evenly'>
-          <button className='text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2' onClick={() => donateToCampaign(index, donationValues[index])}>DONATE</button>
-              <button className='text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 me-2 mb-2' onClick={() => handleWithdraw(index)}>Withdraw</button>
-            </div>
+            <button className='text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2' onClick={() => donateToCampaign(index, donationValues[index])}>DONATE</button>
+            <button className='text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 me-2 mb-2' onClick={() => handleWithdraw(index)}>Withdraw</button>
+          </div>
         </div>
       ))}
 
