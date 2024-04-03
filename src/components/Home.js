f31dd08364d5841472504362289794e9e57faced
Aiswarya K K <86478839+aiswaryaaish@users.noreply.github.com>
2024-04-03 17:46:37 +0530

    Added frontend react code

---
+++ src/components/Home.js
@@ -0,0 +1,34 @@
+import react from "react"
+import Navbar from "./Navbar"
+import BannerBackground from "../Assets/home-banner-background.png";
+import BannerImage from '../Assets/202.png';
+import { FiArrowRight } from "react-icons/fi";
+
+const Home = () => {
+    return (
+      <div className="home-container">
+        <Navbar />
+        <div className="home-banner-container">
+          <div className="home-bannerImage-container">
+            <img src={BannerBackground} alt="" />
+          </div>
+          <div className="home-text-section">
+            <h1 className="primary-heading">
+              de-Caritas
+            </h1>
+            <p className="primary-text">
+              A blockchain based donation traceability framework
+            </p>
+            <button className="secondary-button">
+              Get Started <FiArrowRight />{" "}
+            </button>
+          </div>
+          <div className="home-image-section">
+            <img src={BannerImage} alt="" />
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+export default Home
